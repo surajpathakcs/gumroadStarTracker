fd4fea3ab1a238439aec6ae816a605f88e99c719
npmzero <surajpathak3430@gmail.com>
2025-04-12 09:25:59 +0545

    repo selection for token

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+HttpClient client = new HttpClient();
+client.DefaultRequestHeaders.UserAgent.ParseAdd("GumroadStarTracker");
+
+while (true){
+    var response = client.GetAsync("https://api.github.com/repos/antiwork/gumroad").Result;
+    string jsonResult = await response.Content.ReadAsStringAsync();
+
+    using var doc = JsonDocument.Parse(jsonResult);
+    Console.WriteLine(jsonResult);
+
+}
