15ec23a88cb69900cba3fbdfae475086dedeebcf
npmzero <surajpathak3430@gmail.com>
2025-04-12 12:51:42 +0545

    count star and automatically star if reached 4949

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ client.DefaultRequestHeaders.UserAgent.ParseAdd("GumroadStarTracker");
 Env.Load();
 string token = Environment.GetEnvironmentVariable("gumroad_access_token");
 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", token);
-
+var starCountGlobal = 0;
 
 while (true){
     var response = client.GetAsync("https://api.github.com/repos/antiwork/gumroad").Result;
@@ -20,12 +20,18 @@ while (true){
     using var doc = JsonDocument.Parse(jsonResult);
     var starCount = doc.RootElement.GetProperty("stargazers_count").GetInt32();
 
-    Console.WriteLine("starCount "+starCount);
+    Console.WriteLine("starCount " + starCount);
 
-    await Task.Delay(10000);
+    starCountGlobal = starCount;
+    await Task.Delay(5000);
 
-    if(starCount >= 4997 ){
-        var startResponse = await client.GetAsync("https://api.github.com/user/starred/gumroad/gumroad" , new StringContent());
+    var hasStarred = false;
+    if(starCount == 4949 && !hasStarred){
+        var startResponse = await client.PutAsync("https://api.github.com/user/starred/antiwork/gumroad" , new StringContent(""));
+        Console.WriteLine("Starred status " + startResponse.StatusCode);
+        hasStarred = true;
+        break;
     }
 
 }
+Console.WriteLine("Congrats You are the " + starCountGlobal +"th Stargazer...");
