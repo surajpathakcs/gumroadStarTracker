c8399bedb089be48d996f949324a5a07c362cc50
npmzero <surajpathak3430@gmail.com>
2025-04-12 09:56:50 +0545

    env file setup

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
 using System.Text.Json;
-using System.Text.Json.Serialization;
+using DotNetEnv;
+
 
 HttpClient client = new HttpClient();
 client.DefaultRequestHeaders.UserAgent.ParseAdd("GumroadStarTracker");
+
+Env.Load();
+string token = Environment.GetEnvironmentVariable("gumroad_access_token");
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", token);
+
 
 while (true){
     var response = client.GetAsync("https://api.github.com/repos/antiwork/gumroad").Result;
     string jsonResult = await response.Content.ReadAsStringAsync();
 
+    
     using var doc = JsonDocument.Parse(jsonResult);
-    Console.WriteLine(jsonResult);
+    var starCount = doc.RootElement.GetProperty("stargazers_count").GetInt32();
+
+    Console.WriteLine("starCount "+starCount);
+
+    await Task.Delay(10000);
+
+    if(starCount >= 4997 ){
+        var startResponse = await client.GetAsync("https://api.github.com/user/starred/gumroad/gumroad" , new StringContent());
+    }
 
 }
